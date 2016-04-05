c1a15103e494a5f8ee0e25ce24bf96ea493faf0e
Brad Klein <bradley.klein@twcable.com>
2016-04-05 11:39:10 -0600

    Change archive declaration

--- manifests/init.pp
+++ manifests/init.pp
@@ -119,16 +119,14 @@ class kafka (
   }
 
   archive { "${package_dir}/${basefilename}":
-    ensure          => present,
-    extract         => true,
-    extract_command => 'tar xfz %s --strip-components=1',
-    extract_path    => $install_directory,
-    source          => $package_url,
-    creates         => "${install_directory}/config",
-    cleanup         => true,
-    user            => 'kafka',
-    group           => 'kafka',
-    require         => [
+    checksum         => false,
+    ensure           => present,
+    src_target       => '/',
+    strip_components => 1,
+    target           => $install_directory,
+    url              => $package_url,
+    user             => 'kafka',
+    require          => [
       File[$package_dir],
       File[$install_directory],
       Group['kafka'],
